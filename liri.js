53d9ac8224f0759869d2738869f6130f9df4486b
Karina Khachatryan <karinakhachat@Karinas-MacBook-Air.local>
2018-04-13 10:29:03 -0400

    liri spotify

--- liri.js
+++ liri.js
@@ -6,8 +6,8 @@ var request = require("request");
 var fs = require("fs");
 
 
-//var spotify = new Spotify(keys.spotify);
-//var client = new Twitter(keys.twitter);
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
 
 //four different functions, one for each. one will be a process.argv, 
 
@@ -34,7 +34,7 @@ if (userComand === "movie-this") {
         });
     }
 }
-if (userPick === null){
+if (userPick === null) {
 
     var queryUrl = "http://www.omdbapi.com/?t='Mr.Nobody'&y=&plot=short&apikey=trilogy";
     request(queryUrl, function (error, response, body) {
@@ -47,5 +47,43 @@ if (userPick === null){
     });
 }
 
+if (userComand == "my-tweets") {
+    var params = { screen_name: 'RoseColoredNews', count: 20 };
+    twitter.get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterapi", params, function (error, tweets, response) {
+        if (!error) {
+            console.log(tweets);
+        }
+    },
+        fs.appendFile("random.txt", ", -" + tweets, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+        })
 
-       
+
+    )
+}
+
+if (userComand == "spotify-this") {
+    function Spotify() {
+        spotify.search({
+            type: 'track',
+            query: title,
+            limit: 1,
+        }, function (err, data) {
+            if (data) {
+                var info = data.tracks.items
+                var Spotifyinfo =
+                    "\nSong title: " + info[0].name +
+                    "\nAlbum name: " + info[0].album.name +
+                    "\nURL Preview: " + info[0].preview_url +
+                    console.log(Spotifyinfo)
+                fs.appendFile("log.txt", logSpotify, function (err) {
+                    if (err) {
+                        return console.log("No song has been Spotified!");
+                    };
+                });
+            }
+        })
+    }
+}
